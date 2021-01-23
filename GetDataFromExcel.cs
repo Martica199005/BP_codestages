5c0916166b8ad496de9796003dab9cc6c5a985db
Martica <58595345+Martica199005@users.noreply.github.com>
2021-01-23 22:37:30 +0100

    Create GetDataFromExcel.cs

---
+++ GetDataFromExcel.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace csharp
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opfd = new OpenFileDialog();
+            if (opfd.ShowDialog() == DialogResult.OK)
+                textselect.Text = opfd.FileName;
+        }
+
+        private void showdata_Click(object sender, EventArgs e)
+        {
+            string stringconn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + textselect.Text + ";Extended Properties=\"Excel 8.0;HDR=Yes;\";";
+            OleDbConnection conn = new OleDbConnection(stringconn);
+            if (textselect.Text != "")
+            {
+                OleDbDataAdapter da = new OleDbDataAdapter("Select * from [" + textchoice.Text + "$]", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            else
+                MessageBox.Show("ER");
+        }
+    }
+}
