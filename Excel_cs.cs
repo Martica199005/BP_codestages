c82e702c4f65c92583b255196d5accb1c8ec6b0c
Martica <58595345+Martica199005@users.noreply.github.com>
2021-01-23 17:17:07 +0100

    Update Excel_cs.cs

--- Excel_cs.cs
+++ Excel_cs.cs
@@ -3,3 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel=Microsoft.Office.Interop.Excel;
+
+
+namespace WindowsFormsApp2{
+  class Excel{
+      string path="";
+      _Application excel= new _Excel.Application();
+      Workbook wb;
+      Worksheet ws;
+    
+      //To open the Excel
+      public Excel(string path, int Sheet){
+        this.path=path;
+        wb= excel.Workbooks.Open(path);
+        ws= excel.Worksheets[Sheet]; // Sheet is the index of the sheets
+      }
+    
+    
+      //To read the Excel
+    
+      
+  }
+
+}
