3a15c1108b3aa2337859113a9c040cf83889c663
Martica <58595345+Martica199005@users.noreply.github.com>
2021-01-25 18:00:35 +0100

    Update WriteCollectionToExcel.cs

--- WriteCollectionToExcel.cs
+++ WriteCollectionToExcel.cs
@@ -1,12 +1,11 @@
 // BP Code stage to write a collection to an Excel file
 //It copies also the empty rows
-
+// Input: dt collection, path_file string, start_r and start_c number, Sheet_number number
 
 
 
 Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
 Microsoft.Office.Interop.Excel.Workbook wb = excelApp.Workbooks.Open(path_file);
-//Microsoft.Office.Interop.Excel.Worksheet workSheet = excelApp.ActiveSheet as Microsoft.Office.Interop.Excel.Worksheet;
 Microsoft.Office.Interop.Excel.Worksheet workSheet = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets[Decimal.ToInt32(Sheet_number)]; // define in which worksheet, do you want to add data
 
 if (dt.Rows.Count!=0)
