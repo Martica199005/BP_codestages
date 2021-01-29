dbb7ebcf77eb2230d54f75a4ca2258a885791491
Martica <58595345+Martica199005@users.noreply.github.com>
2021-01-29 20:17:12 +0100

    Update SortCollectionText.cs

--- SortCollectionText.cs
+++ SortCollectionText.cs
@@ -1,5 +1,39 @@
-#Do conversion from string to int
+//Do conversion from string to int
+//input: dt DataTable, Field string, order_desc bool
+//output dt_out DataTable, Success bool
 
+
+try 
+{
+	dt_out=null;
+	Success=true;
+	//add column and convert to int
+	dt.Columns.Add("StrInt", typeof(int));
+	foreach( DataRow dr in dt.Rows){
+	 	dr["StrInt"]=Convert.ToInt32(dr[Field]);
+	 }
+	// sort collection by field
+	string Field="StrInt";
+	string order_desc_str="";
+	if(order_desc==true)
+		order_desc_str=" desc";
+	if (dt.Rows.Count!=0)
+	{
+		DataView dv = dt.DefaultView;
+		dv.Sort = Field+order_desc_str;
+		dt_out = dv.ToTable();
+		
+	}
+}
+catch (Exception e)
+{
+  Success=false;
+  dt_out=null;
+}
+
+
+
+/*
 try 
 {
 	dt_out=null;
@@ -19,4 +53,4 @@ catch (Exception e)
 {
   Success=false;
   dt_out=null;
-}
+}*/
