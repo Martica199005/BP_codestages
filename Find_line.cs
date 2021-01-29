ca0182018d8ed349e8c6ef99caaa30fd7237ddf1
Martica <58595345+Martica199005@users.noreply.github.com>
2021-01-29 22:58:16 +0100

    Create Find_line.cs

---
+++ Find_line.cs
@@ -0,0 +1,17 @@
+
+//Find line
+
+string line;
+found="empty";
+
+
+using(System.IO.StreamReader file =  new System.IO.StreamReader(path_file))
+{
+   while((line = file.ReadLine()) != null)
+   {
+      if(line.Contains(word))
+      {
+		  found=line.Trim();
+      }
+   }
+}
