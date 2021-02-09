96ec8a655503bae584ad124e6fd6718f2c67ade8
Martica <58595345+Martica199005@users.noreply.github.com>
2021-02-09 12:47:39 +0100

    Update ModifyColumns.cs

--- Code_stages/ModifyColumns.cs
+++ Code_stages/ModifyColumns.cs
@@ -2,20 +2,34 @@ try
 {
 	Exists=false;
 	Success=true;
+	dt_out=null;
+
+	List<string> columnNames = new List<string>();
 	foreach(DataColumn dc in dt.Columns)
 	{
-		string ColumnName = dc.ColumnName.ToString();
-	 	if(ColumnName=="DIFFERENZA" && ColumnName=="DIFF_F")
-			Exists=true;
-			dt.Columns["DIFF_F"].SetOrdinal(7);
-			dt.Columns.Remove("DIFFERENZA");
-			dt.Columns["DIFF_F"].ColumnName = "DIFFERENZA";
-			
+		string colName=dc.ColumnName.ToString();
+		columnNames.Add(colName);
+		
 	 }
+	if(columnNames.Contains("DIFFERENZA") && columnNames.Contains("DIFF_F"))
+	{
+		Exists=true;
+		int index=dt.Columns["DIFFERENZA"].Ordinal;
+		DataView dv = dt.DefaultView;
+		dt_out = dv.ToTable();
+		dt_out.Columns["DIFF_F"].SetOrdinal(index);
+		dt_out.Columns.Remove("DIFFERENZA");
+		dt_out.Columns["DIFF_F"].ColumnName = "DIFFERENZA";
+		
+		
+	}
+
 }
 catch (Exception e)
 {
   Success=false;
   Exists=false;
+  dt_out=null;
+
 
 }
