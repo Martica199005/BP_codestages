919d9bdba585795e5fc31d7ed1c45754803434e6
Martica <58595345+Martica199005@users.noreply.github.com>
2021-03-24 17:32:12 +0100

    Update Give_Column_name_by_index.cs

--- Code_stages/Give_Column_name_by_index.cs
+++ Code_stages/Give_Column_name_by_index.cs
@@ -1,1 +1,22 @@
+// input: dt (DataTable), index (Number)
+// output result (Text)
+result="";
+Success=true;
 
+try
+{
+	
+	if(dt.Rows.Count>0){
+		foreach(DataColumn dc in dt.Columns){
+			if (dc.Ordinal == index){
+				result=dc.ColumnName;
+				}
+			}
+		
+	}
+}
+catch (Exception e)
+{
+  Success=false;
+
+}
